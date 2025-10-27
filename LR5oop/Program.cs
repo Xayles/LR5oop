6a5612c517406704d129d873aa9c34a1dc29d13d
Xayles <113243598+Xayles@users.noreply.github.com>
2025-10-27 23:02:46 +0200

    Добавьте файлы проекта.

---
+++ LR5oop/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        // --- 1. Введення номера студента ---
+        Console.Write("Введіть свій номер у журналі K: ");
+        int K = int.Parse(Console.ReadLine());
+
+        // --- 2. Обчислення розміру масиву ---
+        int N = (int)(20 + 0.6 * K);
+        Console.WriteLine($"Розмір масиву N = {N}");
+
+        // --- 3. Заповнення масиву випадковими числами (10-100) ---
+        int[] arr = new int[N];
+        Random rnd = new Random();
+        for (int i = 0; i < N; i++)
+            arr[i] = rnd.Next(10, 101);
+
+        Console.WriteLine("\nПочатковий масив:");
+        PrintArray(arr);
+
+        // --- 4. Сортування вставками ---
+        InsertionSort(arr);
+
+        Console.WriteLine("\nВідсортований масив (методом вставок):");
+        PrintArray(arr);
+
+        // --- 5. Введення ключового значення ---
+        Console.Write("\nВведіть ключове значення для пошуку: ");
+        int key = int.Parse(Console.ReadLine());
+
+        // --- 6. Підрахунок кількості входжень ---
+        int count = CountOccurrences(arr, key);
+        Console.WriteLine($"Ключ {key} зустрічається {count} раз(и).");
+
+        // --- 7. Пошук (бінарний метод) ---
+        int index = BinarySearch(arr, key);
+
+        if (index != -1)
+            Console.WriteLine($"Елемент {key} знайдено на позиції {index + 1}.");
+        else
+            Console.WriteLine($"Елемент {key} не знайдено у масиві.");
+    }
+
+    // --- Метод виведення масиву ---
+    static void PrintArray(int[] arr)
+    {
+        foreach (int x in arr)
+            Console.Write(x + " ");
+        Console.WriteLine();
+    }
+
+    // --- Метод сортування вставками ---
+    static void InsertionSort(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    // --- Метод підрахунку кількості входжень ---
+    static int CountOccurrences(int[] arr, int key)
+    {
+        int count = 0;
+        foreach (int x in arr)
+            if (x == key) count++;
+        return count;
+    }
+
+    // --- Метод бінарного пошуку ---
+    static int BinarySearch(int[] arr, int key)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] < key)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
+    }
+}
